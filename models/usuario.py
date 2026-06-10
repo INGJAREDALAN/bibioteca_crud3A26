2f4b30538b1fe0e1dba130bd1ef95fdc9cdc03ed
INGJAREDALAN <perezpichardojaredalan2@gmail.com>
2026-06-10 12:47:24 -0600

    Modelos de Autor y Carrera

--- models/usuario.py
+++ models/usuario.py
@@ -1,9 +1,10 @@
 class Usuario:
 
     # Constructor 
-    def __init__(self, id_usuario, nombre, email, carrera):
+    def __init__(self, id_usuario, nombre, matricula, email, carrera):
         self.id_usuario = id_usuario
         self.nombre = nombre
+        self.matricula = matricula
         self.email = email
         self.carrera = carrera
         self.activo = True
@@ -15,4 +16,4 @@ class Usuario:
         self.activo = False
 
     def mostar_info(self):
-        return f"Usuario ID: {self.id_usuario}, Nombre: {self.nombre}, Email: {self.email}, Carrera: {self.carrera}, Activo: {'Si' if self.activo else 'No'}"
+        return f"Usuario ID: {self.id_usuario}, Nombre: {self.nombre}, Matricula: {self.matricula}, Email: {self.email}, Carrera: {self.carrera}, Activo: {'Si' if self.activo else 'No'}"
