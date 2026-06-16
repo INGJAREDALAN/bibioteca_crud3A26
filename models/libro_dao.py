6ed696a6e218ce6ec60512bc10129d9c2dc26af8
INGJAREDALAN <perezpichardojaredalan2@gmail.com>
2026-06-16 10:27:08 -0600

    Funciones completas de LibroDAO

---
+++ models/libro_dao.py
@@ -0,0 +1,85 @@
+# DAO: Data Access Object
+# Libro_dao: Objeto de acceso a datos de la tabla libro 
+
+from database.conexion import Conexion
+from models.libro import Libro
+
+class LibroDAO: 
+
+    # SELECT * FROM Libro
+    def obtener_todos(self):
+        conexion = Conexion.obtener_conexion()
+        cursor = conexion.cursor()
+
+        cursor.execute("SELECT *FROM libro")
+        registros = cursor.fetchall()
+
+        libros = []
+        for registro in registros:
+            libro = Libro(
+            registro.id, 
+            registro.titulo,
+            registro.autor,
+            registro.isbn,
+            registro.disponible)
+            libros.append(libro)
+        cursor.close()
+        conexion.close()
+        return libros
+    
+    def insertar(self, libro):
+        conexion = Conexion.obtener_conexion()
+        cursor = conexion.cursor()
+        sql= """
+        INSERT INTO libro (titulo, autor,isbn, dispoonible)
+        VALUES (%s, %s, %s, %s)
+        """
+
+        cursor.execute(
+            sql,
+            (libro.libro,
+            libro.autor,
+            libro.isbn,
+            libro.disponible)
+        )
+
+        conexion.commit()
+        cursor.close()
+        conexion.close()
+
+    def actualizar(self, libro):
+        conexion = Conexion.obtener_conexion()
+        cursor = conexion.cursor()
+
+        sql = """
+        UPDATE libro
+        SET titulo = %s, autor = %s, isbn = %s, 
+        disponible = %s
+        WHERE id = %s
+        """
+
+        cursor.execute(
+            sql,
+            (libro.libro,
+            libro.autor,
+            libro.isbn,
+            libro.disponible,
+            libro.id)
+        )
+
+        conexion.commit()
+        cursor.close()
+        conexion.close()
+
+        def eliminar(self,libro_id):
+            conexion = Conexion.obtener_conexion()
+            cursor = conexion.cursor()
+
+            cursor.execute(" " 
+                "DELETE FROM libro WHERE id = %s",
+                (libro_id,)
+                )
+            
+            conexion.commit()
+            cursor.close()
+            conexion.close()
